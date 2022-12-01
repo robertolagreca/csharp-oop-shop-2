c6dcca7e2caa3f302a6961aaaeb1f2ad74e350fb
robertolagreca <robertolagreca92@gmail.com>
2022-12-01 17:43:58 +0100

    Inizio sottoclasse Cibo in scatola

---
+++ CSharpShop2/CiboScatola.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpShop2
+{
+    public class CiboScatola: Prodotto
+    {
+        private string foodType;
+        private float weight;
+
+
+        public CiboScatola(string name, string description, float price, int iva, string foodType,float weight): base(name, description, price, iva)
+        {
+            this.foodType = foodType;
+            this.weight = weight;
+        }
+    }
+}
